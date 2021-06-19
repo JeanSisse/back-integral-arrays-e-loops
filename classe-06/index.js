ee0f71953c86e481e4e3ce670bff95c5237ca62b
JeanSisse <tavares1988@live.com>
2021-06-19 12:07:04 -0300

    Exercicio 'Soma dos pares' resolvido

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,2 +1,14 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
 
+function somando_pares(arrayDeNumeros) {
+	let somaTotal = 0;
+
+	for (let x of arrayDeNumeros){
+		if (x % 2 === 0) {
+			somaTotal += x;
+		}
+	}
+	return somaTotal;
+}
+
+console.log(somando_pares(numeros));
