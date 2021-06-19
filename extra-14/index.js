10a96d456428ec45b2e881efbe8ac5631781c436
JeanSisse <tavares1988@live.com>
2021-06-19 14:18:22 -0300

    Exercicio extra 'Imprima os menores' resolvido

--- extra-14/index.js
+++ extra-14/index.js
@@ -1,3 +1,13 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+function imprima_menor_da_posicao_entre_arrays(arrayA, arrayB) {
+	for (let x = 0; x < arrayA.length; x++){
+		if (arrayA[x] < arrayB[x])
+			console.log(arrayA[x]);
+		else
+			console.log(arrayB[x]);
+	}
+}
+
+imprima_menor_da_posicao_entre_arrays(arrayA, arrayB);
