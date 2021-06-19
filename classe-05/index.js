6fa36ff9bcf2814f280e08ab5353b195c54541fc
JeanSisse <tavares1988@live.com>
2021-06-19 12:03:39 -0300

    Exercicio 'Filtrando apenas os pares' resolvido

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,1 +1,15 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+
+
+function filtrar_numero_pares(arrayOriginal) {
+	let newArray = [];
+	
+	for (let x of arrayOriginal){
+		if (x%2 === 0) {
+			newArray.push(x);
+		}
+	}
+	return newArray;
+}
+
+console.log(filtrar_numero_pares(original));
