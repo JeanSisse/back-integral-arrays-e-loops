dc2ec6946f4507d42fabd47a859e5c94f111b046
JeanSisse <tavares1988@live.com>
2021-06-19 14:11:03 -0300

    Exercicio exetra 'Verificando disjuntores' resolvido

--- extra-13/index.js
+++ extra-13/index.js
@@ -1,2 +1,11 @@
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+function verificando_disjuntores(disjuntores) {
+	for (let x = 0; x < disjuntores.length; x++){
+		if (disjuntores[x]) {
+			console.log(x)
+		}
+	}
+}
+
+verificando_disjuntores(disjuntores);
