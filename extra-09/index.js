2f4c26132fe5c1122d6d69b647e25b695b503564
JeanSisse <tavares1988@live.com>
2021-06-19 12:31:44 -0300

    Exercicio extra 'Fila de pedidos' resolvido

--- extra-09/index.js
+++ extra-09/index.js
@@ -1,2 +1,12 @@
 
 const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
+
+function fila_de_pedidos(pedido, filaDePedidos) {
+	filaDePedidos.push(pedido);
+	filaDePedidos.shift();
+
+	return filaDePedidos;
+}
+
+
+console.log(fila_de_pedidos("pedido 15", filaDePedidos));
