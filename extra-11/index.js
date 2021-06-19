d7ab6a86c097a59a9ae9e307a31e8bea0c2cfbc1
JeanSisse <tavares1988@live.com>
2021-06-19 12:45:56 -0300

    Exercico extra 'Filtrando números' resolvido

--- extra-11/index.js
+++ extra-11/index.js
@@ -1,2 +1,15 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
 
+function filtrar_numeros(arrayDeNumeros) {
+	let arrayEspecial = [];
+
+	for (let x of arrayDeNumeros){
+		if ((x > 10 && x < 20) || x > 100) {
+			arrayEspecial.push(x);
+		}
+	}
+
+	return arrayEspecial;
+}
+
+console.log(filtrar_numeros(original));
