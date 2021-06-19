f0a46f6cdcbe1c4bf8bd4102928d191e6d944d98
JeanSisse <tavares1988@live.com>
2021-06-19 12:18:25 -0300

    Exercicio 'Filtrando nomes com a letra A' resolvido

--- classe-07/index.js
+++ classe-07/index.js
@@ -1,1 +1,14 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+
+function filtrar_nomes_com_A(arrayNomes) {
+	let nomesComA = [];
+
+	for (let nome of arrayNomes){
+		if (nome.charAt(0) === "A" || nome.charAt(0) === "a") {
+			nomesComA.push(nome);
+		}
+	}
+	return nomesComA;
+}
+
+console.log(filtrar_nomes_com_A(nomes));
