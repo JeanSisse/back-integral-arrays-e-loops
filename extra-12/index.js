7ee66b8a20d448a409869ea9ebe2ebcbb8e97709
JeanSisse <tavares1988@live.com>
2021-06-19 13:53:54 -0300

    Exercicio extra 'Fila do banco' resolvido

--- extra-12/index.js
+++ extra-12/index.js
@@ -1,3 +1,15 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+function fila_de_banco(filaDeDentro, filaDeFora) {
+	
+	while(filaDeFora.length > 0 && filaDeDentro.length < 5){
+		filaDeDentro.push(filaDeFora[0]);
+		filaDeFora.shift();
+	}
+
+	console.log(filaDeDentro);
+	console.log(filaDeFora);
+}
+
+fila_de_banco(filaDeDentro, filaDeFora);
