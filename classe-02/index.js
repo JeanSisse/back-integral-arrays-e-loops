d4c0ce144e554569ecc963b244db842ed1cc3030
JeanSisse <tavares1988@live.com>
2021-06-19 10:59:13 -0300

    Exercicio 'Contando letras' resolvido

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,1 +1,19 @@
 const letras = ["A", "a", "B", "C", "E", "e"];
+let contador = 0;
+
+function contando_letras(letras) {
+	for (x of letras){
+		if (x === "E" || x === "e") {
+			contador++;
+		}
+	}
+
+	if (contador == 0) 
+		console.log(`Nenhuma letra "E" ou "e" foi encontrada.`);
+	else if (contador > 1)
+		console.log(`Foram encontradas ${contador} letras "E" ou "e".`);
+	else
+		console.log(`Foi encontrada ${contador} letra "E" ou "e".`);
+}
+
+contando_letras(letras);
