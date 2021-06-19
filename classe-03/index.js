6732f1f2ace71292a40a7d151a237cd9bc18ab12
JeanSisse <tavares1988@live.com>
2021-06-19 11:08:12 -0300

    Exercicio 'Encontre o 10' resolvido

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,2 +1,14 @@
 const numeros = [54, 22, 14, 87, 10, 284];
+let encontrado = false;
 
+function encontre_o_numero(arrayDeNumeros) {
+	for (let x = 0; x < arrayDeNumeros.length; x++){
+		if (arrayDeNumeros[x] === 10) {
+			return x;
+		}
+	}
+
+	return -1;
+}
+
+console.log(encontre_o_numero(numeros));
