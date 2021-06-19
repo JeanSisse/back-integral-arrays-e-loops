359fcc7618c70cbffefdff3dcfb7f5c830813b9d
JeanSisse <tavares1988@live.com>
2021-06-19 13:23:34 -0300

    Exercicio extra 'Separando pares de ímpares' resolvido

--- extra-15/index.js
+++ extra-15/index.js
@@ -2,3 +2,36 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+function separar_pares_dos_impares(original, pares, impares) {
+	
+	for(let x of original){
+		if (x % 2 === 0)
+			pares.push(x);
+		else
+			impares.push(x);
+	}
+	console.log(pares)
+	console.log(impares);
+
+	// for(let x = 0; x < original.length; x++){
+	// 	if (original[x] % 2 === 0)
+	// 		pares.push(x);
+	// 	else
+	// 		impares.push(x);
+	// }
+	// console.log(pares)
+	// console.log(impares);
+
+	// let x = 0;
+	// while(x < (original.length - 1)){
+	// 	if (original[x] % 2 === 0)
+	// 		pares.push(x);
+	// 	else
+	// 		impares.push(x);
+	// 	x++;
+	// }
+	// console.log(pares)
+	// console.log(impares);
+}
+
+separar_pares_dos_impares(original, pares, impares);
