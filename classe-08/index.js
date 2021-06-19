17fc8bcc6bd24e97e0938c033034abeddc1b0a7a
JeanSisse <tavares1988@live.com>
2021-06-19 12:22:43 -0300

    Exercicio 'Encontrando o maior' resolvido

--- classe-08/index.js
+++ classe-08/index.js
@@ -1,1 +1,15 @@
 const numeros = [3, 24, 1, 8, 11, 7, 15];
+
+function encontre_o_maior(arrayDeNumeros) {
+	let maior = -10000000000;
+
+	for (let x of arrayDeNumeros){
+		if (x > maior){
+			maior = x;
+		}
+	}
+
+	return maior;
+}
+
+console.log(encontre_o_maior(numeros));
